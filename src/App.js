310dcbf44375fb44c5087070dc981c6f56e270ef
Mücahit ÇIĞCİ <86369604+mucahitcigci@users.noreply.github.com>
2023-01-27 21:27:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DataApi from "./pages/DataApi";
+// import DataApi from "./pages/DataApi";
 import QuizApp from "./pages/QuizApp";
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route path="/dataApi" element={<DataApi />} />
+//         <Route path="/dataApi" element={<DataApi />} />
         <Route path="/quiz" element={<QuizApp />} />
       </Routes>
     </BrowserRouter>
