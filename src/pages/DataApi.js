7939221fb4f0b7ecd9ca89edbd97692407dc64bb
Mücahit ÇIĞCİ <86369604+mucahitcigci@users.noreply.github.com>
2023-01-27 21:29:13 +0300

    Update DataApi.js

--- src/pages/DataApi.js
+++ src/pages/DataApi.js
@@ -1,45 +1,45 @@
-import React from "react";
-import JSONDATA from "../MOCK_DATA.json";
-import { useState } from "react";
-import "./DataApi.css";
-import HomeButton from "../componenet/HomeButton";
-function DataApi() {
-  const [searchTerm, setSearchTerm] = useState("");
-  return (
-    <div className="container">
-      <HomeButton />
-      <h2>Search Filter</h2>
-      <h3>Summary</h3>
-      <p>
-        Topics covered: React components, useState(), using controlled HTML
-        input field in React.
-      </p>
-      <div className="data-api">
-        <input
-          className="search-input"
-          type="text"
-          placeholder="Filter..."
-          onChange={(event) => {
-            setSearchTerm(event.target.value);
-          }}
-        />
-      </div>
-      {JSONDATA.filter((val) => {
-        if (searchTerm == "") {
-          return val;
-        } else if (
-          val.first_name.toLowerCase().includes(searchTerm.toLowerCase())
-        ) {
-          return val;
-        }
-      }).map((val, key) => {
-        return (
-          <div className="user" key={key}>
-            <p>{val.first_name}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
-export default DataApi;
+// import React from "react";
+// import JSONDATA from "../MOCK_DATA.json";
+// import { useState } from "react";
+// import "./DataApi.css";
+// import HomeButton from "../componenet/HomeButton";
+// function DataApi() {
+//   const [searchTerm, setSearchTerm] = useState("");
+//   return (
+//     <div className="container">
+//       <HomeButton />
+//       <h2>Search Filter</h2>
+//       <h3>Summary</h3>
+//       <p>
+//         Topics covered: React components, useState(), using controlled HTML
+//         input field in React.
+//       </p>
+//       <div className="data-api">
+//         <input
+//           className="search-input"
+//           type="text"
+//           placeholder="Filter..."
+//           onChange={(event) => {
+//             setSearchTerm(event.target.value);
+//           }}
+//         />
+//       </div>
+//       {JSONDATA.filter((val) => {
+//         if (searchTerm == "") {
+//           return val;
+//         } else if (
+//           val.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+//         ) {
+//           return val;
+//         }
+//       }).map((val, key) => {
+//         return (
+//           <div className="user" key={key}>
+//             <p>{val.first_name}</p>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+// export default DataApi;
